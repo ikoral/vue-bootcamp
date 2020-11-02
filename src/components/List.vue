0a51b09672d14bb26c54b9c13b0ee5c8b33a6475
ikoral <ilhankoral@outlook.com>
2020-11-02 01:38:27 +0100

    primitive todo app

---
+++ src/components/List.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+    <ul v-for = "(item, itemId) in childItems" :key = "itemId">
+        <li> 
+            {{item}}
+        </li>
+    </ul>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'List',
+    props: ['childItems']
+}
+</script>
+
+<style scoped>
+
+</style>
