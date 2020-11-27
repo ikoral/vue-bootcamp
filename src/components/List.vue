1924a84c262630e694579b119cfd40defc12812a
ikoral <ilhankoral@outlook.com>
2020-11-27 20:11:33 +0100

    todo list updated with inline emit

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class = "mt-4 lg:w-1/2 w-4/5">
+  <div class="mt-4 lg:w-1/2 w-4/5">
     <ul v-for="(item, itemId) in childItems" :key="itemId">
       <li>
-        <div>{{ item.name }}</div>
-        <!-- <button @click.prevent="$emit('remove-item', itemId)">X</button>  -->
-        <button @click.prevent="doTest">X</button> 
+        <div class="flex items-center">
+          <input class="mr-2" type="checkbox" />
+          <div>{{ item.name }}</div>
+        </div>
+        <button @click.prevent="$emit('remove-item', itemId)">X</button>
+        <!-- <button @click.prevent="doTest">X</button>  -->
       </li>
     </ul>
   </div>
@@ -14,24 +17,24 @@
 export default {
   name: "List",
   props: ["childItems"],
-  methods:{
-      doTest: function(){
-          console.log(this.itemId)
-      }
-  }
+  methods: {
+    doTest: function () {
+      console.log(this.itemId);
+    },
+  },
 };
 </script>
 
 <style scoped>
 li {
-    padding: 5px 15px;
-    margin-bottom: 10px;
-    background: #fff;
-    border-radius: 15px;
-    color: #555;
-    display: flex;
-    justify-content: space-between;
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    font-size: 14px;
+  padding: 5px 15px;
+  margin-bottom: 10px;
+  background: #fff;
+  border-radius: 15px;
+  color: #555;
+  display: flex;
+  justify-content: space-between;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  font-size: 14px;
 }
 </style>
