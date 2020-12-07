ecf9db7c153962524bd9123bb2682691b14fc3a9
ikoral <ilhankoral@outlook.com>
2020-12-07 19:35:45 +0100

    shop cart initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,74 @@
 <template>
   <div class="w-full flex flex-col items-center">
-    <MyButton v-on:got-clicked="handleFormSubmitted" />
-    {{ message }}
-    <div id="sep"></div>
-    <ComponentA />
-    <ComponentB />
+    <h1>The Cup Store</h1>
+    <product
+      v-for="product in products(product, id)"
+      key="index"
+      name="Colorful Cups"
+      :price="999"
+      :image="cups1"
+    ></product>
+
+    <cart></cart>
   </div>
 </template>
 
 <script>
-import MyButton from "./components/MyButton";
-import ComponentA from "./components/ComponentA";
-import ComponentB from "./components/ComponentB";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import Cups1 from "./assets/cups1.jpg";
+import Cups2 from "./assets/cups2.jpg";
+import Cups3 from "./assets/cups3.jpg";
 
 export default {
   name: "App",
   components: {
-    MyButton,
-    ComponentA,
-    ComponentB,
+    Product,
+    Cart,
   },
 
   data: () => {
     return {
-      message: "",
+      cups1: Cups1,
+      cups2: Cups2,
+      cups3: Cups3,
+      products: [
+        {
+          id: 120,
+          name: "Colorful Cups",
+          price: 999,
+          image: "./assets/cups1.jpg",
+        },
+        {
+          id: 121,
+          name: "Coffee & Home",
+          price: 1449,
+          image: "./assets/cups2.jpg",
+        },
+        {
+          id: 122,
+          name: "Old red Friend",
+          price: 1999,
+          image: "./assets/cups3.jpg",
+        },
+      ],
     };
   },
 
-  methods: {
-    handleFormSubmitted: function () {
-      this.message = "CLICKED";
-    },
-  },
+  methods: {},
 };
 </script>
 
 <style>
-#sep {
-  border-bottom: solid 1px #ddd;
-  height: 10px;
-  width: 100%;
+body {
+  padding: 15px 0;
+  font-size: 14px;
+  background: #eeffff;
+  color: #888;
+}
+h1 {
+  text-align: center;
+  font-size: 24px;
+  margin-bottom: 15px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 //We are creating new event bus here
-export const EventBus = new Vue();
 
 new Vue({
   render: (h) => h(App),
